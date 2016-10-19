4f7f37e549446f4d8aded473e6197908f57bd31c
Pete Eigel <pete@eigel.com>
2016-10-19 17:44:57 -0500

    Added rudamentary cli.

---
+++ solver-cli/src/main.cpp
@@ -0,0 +1,43 @@
+#include "sudoku.h"
+#include "sudoku/solver.h"
+#include "sudoku/io.h"
+#include <iostream>
+#include <fstream>
+#include <exception>
+
+using namespace std;
+using namespace sudoku;
+
+int main (int argc, char** argv) {
+	try {
+		char* filenmae = argv[argc - 1];
+		ifstream input (filenmae, ifstream::in);
+		auto puz = io::read_puzzle<3>(input);
+
+		cout << "Puzzle read successfully.\n";
+		io::pretty_print(puz);
+
+		io::enable_logging();
+		auto result = solver::solve(puz, true);
+
+		cout << "\n\n==============\n\n";
+
+		if (result.num_solutions == 1) {
+			cout << "Unique solution found:\n";
+			io::pretty_print(result.solution);
+		} else if (result.num_solutions == 0) {
+			cout << "No solutions found.\n";
+		} else if (result.num_solutions > 1) {
+			cout << result.num_solutions << " solutions found:";
+			io::pretty_print(result.solution);
+		} else {
+			cerr << "Unexpected solver result. Please report this bug.";
+			return 1;
+		}
+
+		return 0;
+	} catch (exception& e) {
+		cerr << e.what() << '\n';
+		return 1;
+	}
+}
